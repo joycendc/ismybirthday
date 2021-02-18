c3ca3bd84579d469bb426f69afa6a54bfa1fb081
joycendc <capilijoycen0@gmail.com>
2021-02-18 12:23:43 +0800

    changed

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 today = mm + "" + dd;
 
-if(today === "0831"){
+if(today === "0401"){
     document.body.style.background = "linear-gradient(to right top, #B22222, #CD5C5C)";
     text.style.color = "white";
     yesno.style.color = "white";
     Fallit();
     
-    text.textContent = "HAPPY BIRTHDAY OICEN 🎉";
-    yesno.textContent = "WISHING YOU A YEAR OF HAPPINESS";
+    text.textContent = "HAPPY BIRTHDAY ANGELIE LOVE 🎉";
+    yesno.textContent = "WISHING YOU A YEAR OF HAPPINESS \n I LOVE YOU SO MUCH  😘 ";
 }else {
     yesno.textContent = "NO"
 }
